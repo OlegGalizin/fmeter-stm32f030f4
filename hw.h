3f7081e58b9d7a655f17825bf0dccf195f7f609c
unknown <galizin@mail.ru>
2015-09-09 14:40:23 +0300

    LCD n1202 hello

---
+++ hw.h
@@ -0,0 +1,16 @@
+#include "gpio.h"
+
+//                          AINPUT  OUT10MHZ                PULL_DOWN   AF0  0
+//                          INPUT   OUT2MHZ  0              PULL_UP     AF7  1
+//                          OUTPUT  OUT50MHZ AUOT_OPENDRAIN 0
+//                          AOUTPUT
+//#define PIN_NAME PORT, PIN, MODE, SPEED,   OUT_MODE,      PULL,      , AF, SET
+#define LCD_CLK   A, 4, OUTPUT, OUT10MHZ, 0, 0, 0, 0
+#define LCD_DA    A, 2, OUTPUT, OUT10MHZ, 0, 0, 0, 0
+#define LCD_RESET A, 3, OUTPUT, OUT10MHZ, 0, 0, 0, 0
+#define LCD_CE    A, 1, OUTPUT, OUT10MHZ, 0, 0, 0, 0
+#define LCD_PWR   A, 0, OUTPUT, OUT10MHZ, 0, 0, 0, 0
+#define SWDIO     A, 13, AOUTPUT, OUT50MHZ, 0, PULL_UP, AF0, 0
+#define SWDCLK    A, 14, AOUTPUT, OUT2MHZ, 0, PULL_DOWN, AF0, 0
+
+#define PINS SWDIO, SWDCLK, LCD_CLK, LCD_DA, LCD_RESET, LCD_CE, LCD_PWR
--- sample.c
+++ sample.c
@@ -1,17 +1,5 @@
-#include "gpio.h"
-
-//                          AINPUT  OUT10MHZ                PULL_DOWN   AF0  0
-//                          INPUT   OUT2MHZ  0              PULL_UP     AF7  1
-//                          OUTPUT  OUT50MHZ AUOT_OPENDRAIN 0
-//                          AOUTPUT
-//#define PIN_NAME PORT, PIN, MODE, SPEED,   OUT_MODE,      PULL,      , AF, SET
-#define RELAY1 A, 4, OUTPUT, OUT10MHZ, 0, 0, 0, 0
-#define RELAY2 A, 3, OUTPUT, OUT10MHZ, 0, 0, 0, 1
-#define RELAY3 B, 0, OUTPUT, OUT10MHZ, 0, 0, 0, 1
-#define SWDIO  A, 13, AOUTPUT, OUT50MHZ, 0, PULL_UP, AF0, 0
-#define SWDCLK A, 14, AOUTPUT, OUT2MHZ, 0, PULL_DOWN, AF0, 0
-
-#define PINS RELAY1, RELAY2, RELAY3, SWDIO, SWDCLK
+#include "hw.h"
+#include "n1202.h"
 
 static delay()
 {
@@ -27,16 +15,14 @@ void main()
 	GPIOA->OSPEEDR = TO_GPIO_OSPEEDR(A, PINS);	
 	GPIOA->PUPDR = TO_GPIO_PUPDR(A, PINS);
 	GPIOA->ODR = TO_GPIO_ODR(A, PINS);
+
+
+
 	GPIOA->AFR[0] = TO_GPIO_AFRL(A, PINS);
-	GPIOA->AFR[1] = TO_GPIO_AFRH(A, PINS);
+  GPIOA->AFR[1] = TO_GPIO_AFRH(A, PINS);
 
-	GPIOB->MODER = TO_GPIO_MODER(B, PINS);
-
-  while(1)
-	{
-		GPIO_SET(RELAY1);
-		delay();
-		GPIO_RESET(RELAY1);
-		delay();
-	}
+  GPIO_SET(LCD_PWR);
+  LcdInit();
+	LcdClear();
+	LcdChr(X_POSITION*3+ Y_POSITION*4 + INVERSE+11, "Hello world");
 }
